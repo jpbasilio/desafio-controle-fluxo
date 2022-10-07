67f3e741e9ef663ccf5d5fffebb1d1ecde663c7a
João Silva <jpsilva313@gmail.com>
2022-10-07 16:17:47 -0300

    finished project

--- src/application/Contador.java
+++ src/application/Contador.java
@@ -1,9 +1,19 @@
 package application;
 
 import exceptions.BusinessException;
-
 import java.util.Scanner;
 
+/**
+ * Controle de Fluxo - Desafio
+ * Vamos exercitar
+ * todo o conteúdo apresentado no módulo de Controle de Fluxo codificando o seguinte cenário.
+ *
+ * O sistema deverá receber dois parâmetros via terminal que representarão dois números inteiros,
+ * com estes dois números você deverá obter a quantidade de interações (for) e realizar a impressão
+ * no console (System.out.print) dos números incrementados
+ *
+ * @author João
+ */
 public class Contador {
     public static void main(String[] args) {
 
